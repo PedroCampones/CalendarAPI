f6ac391ee0cd24876c7c73b0827b4551aadd5732
Pedro Camponês <pedro.campones@arentia.pt>
2021-07-07 17:01:23 +0100

    Add project files.

---
+++ CalendarAPI/Controllers/CalendarController.cs
@@ -0,0 +1,78 @@
+﻿using CalendarAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CalendarAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalendarController : ControllerBase
+    {
+        public static List<Person> Persons = new List<Person>()
+        {
+            new Person()
+            {
+                Name = "Ze",
+                Role = "Interview",
+                Slots = new List<Slot>()
+                {
+                    new Slot()
+                    {
+                        DateStart = DateTime.Now,
+                        DateEnd = DateTime.Now.AddDays(5)
+                    }
+                }
+            }, new Person()
+            {
+                Name = "Pedro",
+                Role = "Candidate",
+                Slots = new List<Slot>()
+                {
+                    new Slot()
+                    {
+                        DateStart = DateTime.Now,
+                        DateEnd = DateTime.Now.AddDays(5)
+                    }
+                }
+            }
+        };
+        public CalendarController()
+        {
+        }
+        [HttpGet]
+        public List<Person> Get()
+        {
+            return Persons.ToList();
+        }
+        [HttpPost]
+        public Person Post(Person person)
+        {
+            if (person != null)
+            {
+                Persons.Add(person);
+            }
+            return person;
+        }
+
+        [HttpGet]
+
+        public List<Slot> Available([FromQuery] string candidate, [FromQuery] List<string> interviewers)
+        {
+            List<Slot> slots = new List<Slot>();
+            if (string.IsNullOrEmpty(candidate) || interviewers.Count == 0) return new List<Slot>();
+            Person personCandidate = Persons.Where(x => x.Name == candidate && x.Role.ToLowerInvariant() == "candidate").FirstOrDefault();
+            if (candidate == null) return new List<Slot>();
+
+            List<Person> personInterviewers = (from persons in Persons
+                                               join inter in interviewers on persons.Name equals inter
+                                               where persons.Role.ToLowerInvariant() == "interview"
+                                               select persons).ToList();
+            if (personInterviewers.Count == 0) return new List<Slot>();
+            personInterviewers.ForEach(x => slots.AddRange(x.Slots));
+            return slots;
+        }
+    }
+}
---
+++ CalendarAPI/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalendarAPI.Models
+{
+    public class Person
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+
+        [Required]
+        public List<Slot> Slots { get; set; }
+    }
+}
